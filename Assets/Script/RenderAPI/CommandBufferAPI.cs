65e531a68f5d3636c8a53afb1ceebd1e9915d859
manaijin <794636514@qq.com>
2022-01-27 18:51:55 +0800

    Unity渲染绘制接口 - CommandBuffer Demo

--- Assets/Script/RenderAPI/CommandBufferAPI.cs
+++ Assets/Script/RenderAPI/CommandBufferAPI.cs
@@ -1,18 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class CommandBufferAPI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Material mat;
+
+    private CommandBuffer buffer1;
+    private CommandBuffer buffer2;
+    private int porary1;
+    private int porary2;
+
     void Start()
     {
-         
+        porary1 = Shader.PropertyToID("tempProperty1");
+        porary2 = Shader.PropertyToID("tempProperty2");
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if (buffer1 == null)
+        {
+            buffer1 = new CommandBuffer() { name = "AfterOpaqueTexture" };
+            Camera.main.AddCommandBuffer(CameraEvent.AfterForwardOpaque, buffer1);
+        }
+
+        buffer1.Clear();
+        buffer1.GetTemporaryRT(porary1, -1, -1, 0, FilterMode.Bilinear);
+        buffer1.Blit(BuiltinRenderTextureType.CurrentActive, porary1);
+        buffer1.SetGlobalTexture("AfterOpaqueTexture", porary1);
+        //buffer1.ReleaseTemporaryRT(porary1);
+
+
+        if (buffer2 == null)
+        {
+            buffer2 = new CommandBuffer() { name = "AfterAllTexture" };
+            Camera.main.AddCommandBuffer(CameraEvent.AfterImageEffects, buffer2);
+        }
+        buffer2.Clear();
+        buffer2.GetTemporaryRT(porary2, -1, -1, 0, FilterMode.Bilinear);
+        buffer2.Blit(BuiltinRenderTextureType.CurrentActive, porary2);
+        buffer2.SetGlobalTexture("AfterAllTexture", porary2);
+        //buffer2.ReleaseTemporaryRT(porary2);
+    }
+
+    private void OnDisable()
+    {
+        Clear();
+    }
+
+    private void OnDestroy()
+    {
+        Clear();
+    }
+
+    void Clear()
+    {
+        if (buffer1 != null)
+        {
+            buffer1.Clear();
+            Camera.main.RemoveCommandBuffer(CameraEvent.AfterForwardOpaque, buffer1);
+            buffer1 = null;
+        }
+
+        if (buffer2 != null)
+        {
+            buffer2.Clear();
+            Camera.main.RemoveCommandBuffer(CameraEvent.AfterForwardOpaque, buffer2);
+            buffer2 = null;
+        }
     }
 }
